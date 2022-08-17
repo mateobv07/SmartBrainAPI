aaa38e5e0846154e4cb3d91416c0931b0806c9c2
unknown <bernasconivargas.mateo@gmail.com>
2022-08-17 17:39:45 -0500

    all

--- server.js
+++ server.js
@@ -36,7 +36,7 @@ app.put('/image', (req, res) => { profile.handleImage(req, res, db) })
 //Clarifai api call
 app.post('/imageurl', (req, res) => { profile.handleApiCall(req, res) })
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 13785;
 
 app.listen(PORT, ()=> {
     console.log(`App is running on port ${PORT}`);
